308fe31ee6d89a5597bf2e6a6e945fc379235d26
Nikolai Prjanikov <prjanikovnikolai@gmail.com>
2022-11-30 20:16:39 +0100

    Removed Printer and Scan functionality

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import app
-import threading
 import sys
 import os
 
 if __name__ == '__main__':
     os.chdir(sys.path[0])
-    if 'noscan' not in sys.argv:
-        import scan
-        scan_thread = threading.Thread(target=scan.run, args=(app.send_code,), daemon=True)
-        scan_thread.start()
     app.run()
