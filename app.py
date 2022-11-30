c1d9a41aa8576373af0d1ec96642bca6881800ef
Nikolai Prjanikov <prjanikovnikolai@gmail.com>
2022-11-30 20:48:45 +0100

    Reduced dependencies

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ monkey.patch_all()
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 import json, atexit, time
 
 USERS_PATH = 'data/users.json'
